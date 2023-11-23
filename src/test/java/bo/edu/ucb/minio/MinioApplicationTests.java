cee8c1cf922ced73ce666fae3bca889f16ffc7e0
imferr <117535778+imferr@users.noreply.github.com>
2023-11-23 19:12:06 -0400

    initial commit

---
+++ src/test/java/bo/edu/ucb/minio/MinioApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
